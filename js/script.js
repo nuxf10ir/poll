3bb5d5494b40dbb1337393caffcdcc07e28c1b4b
Nuxf10ir <siiin@inbox.ru>
2018-10-14 16:24:29 +0300

    init

---
+++ js/script.js
@@ -0,0 +1,90 @@
+_.templateSettings = {
+    evaluate: /\{\[([\s\S]+?)\]\}/g,
+    interpolate: /\{\{([\s\S]+?)\}\}/g
+};
+
+$voteContainer = $("#fd-vote__content");
+loaderTmpl = _.template($("#loader_tmpl").html());
+voteTmpl = _.template($("#vote_tmpl").html());
+
+
+/*
+
+jQuery.fn.cardGame = function(data) {
+    var $self = $(this),
+        questionsData = gameData.questionsData,
+        answersData = gameData.answersData,
+        $card = $("#card", $self),
+        $front = $("#card-front", $card),
+        $back = $("#card-back", $card),
+        questionTmpl = _.template($("#question__tmpl").html()),
+        answerTmpl = _.template($("#answer__tmpl").html()),
+        finishTmpl = _.template($("#finish__tmpl").html());
+
+    $card.flip({
+        trigger: 'manual',
+        axis: 'y'
+    });
+
+    $card.on("click.cardGame", ".js-to-question", function(e) {
+        var $this = $(e.currentTarget),
+            questionId = $this.data("question");
+
+        if (questionId === -1) {
+            $front.html(finishTmpl(_.extend(questionsData[questionId], {id: questionId})));
+        } else {
+            $front.html(questionTmpl(_.extend(questionsData[questionId], {id: questionId})));
+        }
+
+
+        $card.flip(false);
+
+    });
+
+    $card.on("click.cardGame", ".js-to-answer", function(e) {
+        var $this = $(e.currentTarget),
+            questionId = $this.data("question");
+            answerId = $this.data("answer");
+
+        $back.html(answerTmpl(answersData[questionId][answerId]));
+        $card.flip(true);
+    });
+
+
+    return {
+        init: function () {
+            setTimeout(function () {
+                $card.flip(true);
+            }, 1000);
+        }
+    }
+
+
+};*/
+
+
+$voteContainer.html(loaderTmpl());
+
+$.ajax({
+        method: "GET",
+        url: "https://voice.fd.ru/api/get-poll.php"/*,
+        crossDomain: true,
+        xhrFields: {
+            withCredentials: true
+        }*/
+    })
+    .done(function(response) {
+        console.log(response)
+    })
+    .fail(function() {
+        alert( "error" );
+    })
+    .always(function() {
+        alert( "complete" );
+    });
+
+
+
+
+
+
